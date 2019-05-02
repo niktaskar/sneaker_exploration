8e7244f1370754e177abcc0b6a232a1c26d25648
ikatzman <ikatzman@wustl.edu>
2019-05-02 11:59:43 -0500

    added json and setup for main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+function showSneakers() {
+  console.log("sneakers");
+  console.log(event.target.id);
+}
+
+function showSandals() {
+  console.log("sandals");
+}
+
+function showBoots() {
+  console.log("boots");
+}
+
+function showDress() {
+  console.log("dress shoes");
+}
