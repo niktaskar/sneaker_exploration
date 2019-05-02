18c32885a953145137b0834c9e9d8453b39abe21
ikatzman <ikatzman@wustl.edu>
2019-05-02 16:40:21 -0500

    added onClick function for each picture

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-  console.log(data.Shoes.length);
   for(var i = 0; i < 32; i++){
-    console.log(data.Shoes[i]);
-    document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+    // document.getElementById("content-div").innerHTML += '<img id="' + i + '" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+    // document.getElementById(i).addEventListener("click", moreInfo());
+    document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
   }
 });
 
@@ -16,10 +16,8 @@ $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
 function showAll() {
   document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    console.log(data.Shoes.length);
     for(var i = 0; i < 32; i++){
-      console.log(data.Shoes[i]);
-      document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
     }
   });
 }
@@ -27,10 +25,8 @@ function showAll() {
 function showSneakers() {
   document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    console.log(data.Shoes.length);
     for(var i = 0; i < 8; i++){
-      console.log(data.Shoes[i]);
-      document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + data.Shoes[i] + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
     }
   });
 }
@@ -38,10 +34,8 @@ function showSneakers() {
 function showSandals() {
   document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    console.log(data.Shoes.length);
     for(var i = 8; i < 16; i++){
-      console.log(data.Shoes[i]);
-      document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + data.Shoes[i] + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
     }
   });
 }
@@ -49,10 +43,8 @@ function showSandals() {
 function showBoots() {
   document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    console.log(data.Shoes.length);
     for(var i = 16; i < 24; i++){
-      console.log(data.Shoes[i]);
-      document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + data.Shoes[i] + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
     }
   });
 }
@@ -60,19 +52,17 @@ function showBoots() {
 function showSkate() {
   document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    console.log(data.Shoes.length);
     for(var i = 24; i < 32; i++){
-      console.log(data.Shoes[i]);
-      document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + data.Shoes[i] + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
     }
   });
 }
 
-function moreInfo(data) {
-  console.log(data);
+function moreInfo(d) {
+  document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    console.log(data.Shoes[1]);
-    console.log(typeof(data));
-    // document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+    document.getElementById("content-div").innerHTML += '<img class="moreInfoImg" src="' + data.Shoes[d].id + '.jpg" alt="' + data.Shoes[d].name + '">'
+    document.getElementById("content-div").innerHTML += '<p class="shoePrice">$' + data.Shoes[d].price + '</p>'
+    document.getElementById("content-div").innerHTML += '<p class="shoeDesc">' + data.Shoes[d].description + '</p>'
   });
 }
