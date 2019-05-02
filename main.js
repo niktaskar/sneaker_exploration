887c08dbe1edc9de3b206c94d7c705bec11cc040
ikatzman <ikatzman@wustl.edu>
2019-05-02 15:22:43 -0500

    show all button

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ $.getJSON('https://api.myjson.com/bins/1cebo0', function(data) {
 //   }
 // }
 
+function showAll() {
+  document.getElementById("content-div").innerHTML = " ";
+  $.getJSON('https://api.myjson.com/bins/1cebo0', function(data) {
+    console.log(data.Shoes.length);
+    for(var i = 0; i < 32; i++){
+      console.log(data.Shoes[i]);
+      document.getElementById("content-div").innerHTML += '<img onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+    }
+  });
+}
+
 function showSneakers() {
   document.getElementById("content-div").innerHTML = " ";
   $.getJSON('https://api.myjson.com/bins/1cebo0', function(data) {
