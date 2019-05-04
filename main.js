04a63ecd0aef3a8cc4cb083be806f2cde9999cd5
Nikash Taskar <nikashtaskar@wustl.edu>
2019-05-04 13:54:30 -0400

    testing out the apis

--- main.js
+++ main.js
@@ -1,69 +1,114 @@
-$.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-  for(var i = 0; i < 32; i++){
-    // document.getElementById("content-div").innerHTML += '<img id="' + i + '" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-    // document.getElementById(i).addEventListener("click", moreInfo());
-    document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-  }
-});
+// $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+//     for (var i = 0; i < 32; i++) {
+//         // document.getElementById("content-div").innerHTML += '<img id="' + i + '" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
+//         // document.getElementById(i).addEventListener("click", moreInfo());
+//         document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">';
+//     }
+// });
 
-// function init() {
-//   gapi.client.setApiKey("AIzaSyB9xKnolfm9SuuGvC79P5poa7Ejs_zQxBM");
-//   gapi.client.load("youtube", "v3", function() {
-//
-//   }
-// }
+//AIzaSyDiSNpVFbmMh88aexS32k9Qj6vFJ4gDdlg
 
 function showAll() {
-  document.getElementById("content-div").innerHTML = " ";
-  $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    for(var i = 0; i < 32; i++){
-      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-    }
-  });
+    document.getElementById("content-div").innerHTML = " ";
+    $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+        for (var i = 0; i < 32; i++) {
+            document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">';
+        }
+    });
 }
 
 function showSneakers() {
-  document.getElementById("content-div").innerHTML = " ";
-  $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    for(var i = 0; i < 8; i++){
-      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-    }
-  });
+    document.getElementById("content-div").innerHTML = " ";
+    $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+        for (var i = 0; i < 8; i++) {
+            document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">';
+        }
+    });
 }
 
 function showSandals() {
-  document.getElementById("content-div").innerHTML = " ";
-  $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    for(var i = 8; i < 16; i++){
-      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-    }
-  });
+    document.getElementById("content-div").innerHTML = " ";
+    $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+        for (var i = 8; i < 16; i++) {
+            document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">';
+        }
+    });
 }
 
 function showBoots() {
-  document.getElementById("content-div").innerHTML = " ";
-  $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    for(var i = 16; i < 24; i++){
-      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-    }
-  });
+    document.getElementById("content-div").innerHTML = " ";
+    $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+        for (var i = 16; i < 24; i++) {
+            document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">';
+        }
+    });
 }
 
 function showSkate() {
-  document.getElementById("content-div").innerHTML = " ";
-  $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    for(var i = 24; i < 32; i++){
-      document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">'
-    }
-  });
+    document.getElementById("content-div").innerHTML = " ";
+    $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+        for (var i = 24; i < 32; i++) {
+            document.getElementById("content-div").innerHTML += '<img class="listImg" onclick="moreInfo(' + i + ')" src="' + data.Shoes[i].id + '.jpg" alt="' + data.Shoes[i].name + '">';
+        }
+    });
 }
 
 function moreInfo(d) {
-  document.getElementById("content-div").innerHTML = " ";
-  $.getJSON('https://api.myjson.com/bins/gul64', function(data) {
-    document.getElementById("content-div").innerHTML += '<p class="shoeName">' + data.Shoes[d].brand + " " + data.Shoes[d].name + '</p>'
-    document.getElementById("content-div").innerHTML += '<img class="moreInfoImg" src="' + data.Shoes[d].id + '.jpg" alt="' + data.Shoes[d].name + '">'
-    document.getElementById("content-div").innerHTML += '<p class="shoePrice">$' + data.Shoes[d].price + '</p>'
-    document.getElementById("content-div").innerHTML += '<p class="shoeDesc">' + data.Shoes[d].description + '</p>'
-  });
+    document.getElementById("content-div").innerHTML = " ";
+    $.getJSON('https://api.myjson.com/bins/gul64', function (data) {
+        document.getElementById("content-div").innerHTML += '<p class="shoeName">' + data.Shoes[d].brand + " " + data.Shoes[d].name + '</p>';
+        document.getElementById("content-div").innerHTML += '<img class="moreInfoImg" src="' + data.Shoes[d].id + '.jpg" alt="' + data.Shoes[d].name + '">';
+        document.getElementById("content-div").innerHTML += '<p class="shoePrice">$' + data.Shoes[d].price + '</p>';
+        document.getElementById("content-div").innerHTML += '<p class="shoeDesc">' + data.Shoes[d].description + '</p>';
+
+        // loadClient();
+        // // execute(data.Shoes[d].name);
+    });
 }
+
+// function loadClient() {
+//     gapi.client.setApiKey("AIzaSyB9xKnolfm9SuuGvC79P5poa7Ejs_zQxBM");
+//     gapi.client.load("youtube", "v3", function(){
+//         console.log("loaded API");
+//     })
+
+// }
+
+// // Make sure the client is loaded before calling this method.
+// function execute(query) {
+//     return gapi.client.youtube.search.list({
+//         "part": "snippet",
+//         "maxResults": 25,
+//         "q": query
+//     })
+//         .then(function (response) {
+//             // Handle the results here (response.result has the parsed body).
+//             console.log("Response", response);
+//         },
+//             function (err) { console.error("Execute error", err); });
+// }
+// gapi.load("client");
+
+showAll();
+
+function start() {
+    // 2. Initialize the JavaScript client library.
+    gapi.client.init({
+        'apiKey': 'AIzaSyDiSNpVFbmMh88aexS32k9Qj6vFJ4gDdlg'
+        // clientId and scope are optional if auth is not required.
+        // 'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+        // 'scope': 'profile',
+    }).then(function () {
+        // 3. Initialize and make the API request.
+        return gapi.client.request({
+            'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+        })
+    }).then(function (response) {
+        console.log(response.result);
+    }, function (reason) {
+        console.log('Error: ' + reason.result.error.message);
+    });
+};
+// 1. Load the JavaScript client library.
+gapi.load('client', start);
+
